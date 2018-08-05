be20a3c3a46af82c835bbed73e9d2f437f5edfd2
Joe Previte <jjprevite@gmail.com>
2018-08-05 15:51:01 -0700

    add button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { css } from 'emotion'
 
+const buttonStyles = css`
+  background: linear-gradient(
+    0deg,
+    #7fd6ff 27.3%,
+    rgba(127, 229, 255, 0) 173.47%
+  );
+  border-radius: 32px;
+  color: #fff;
+  width: 157px;
+  height: 49px;
+`
 class Button extends Component {
-  render () {
-    return (
-      <button />
-    )
+  render (props) {
+    return <button className={buttonStyles}>{props.text}</button>
   }
 }
 

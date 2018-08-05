10563cdf3801b3f8b2362732341b00b146bea64c
Joe Previte <jjprevite@gmail.com>
2018-08-05 16:18:23 -0700

    change hover color, break out styles into separate file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react'
-import { css } from 'emotion'
-
-const buttonStyles = css`
-  background: linear-gradient(
-    0deg,
-    #7fd6ff 27.3%,
-    rgba(127, 229, 255, 0) 173.47%
-  );
-  border-radius: 32px;
-  color: #fff;
-  width: 157px;
-  height: 49px;
-`
+import { buttonStyles } from './Button.styles'
 class Button extends Component {
-  render (props) {
-    return <button className={buttonStyles}>{props.text}</button>
+  render () {
+    return <button className={buttonStyles}>{this.props.text}</button>
   }
 }
 
---
+++ src/components/Button.styles.js
@@ -0,0 +1,27 @@
+import { css } from 'emotion'
+export const buttonStyles = css`
+  background: linear-gradient(
+    0deg,
+    #2ab7f8 27.3%,
+    rgba(127, 229, 255, 0) 173.47%
+  );
+  border-color: transparent;
+  border-width: 0;
+  border-radius: 32px;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  margin: 30px 0;
+  transition: all 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+  height: 49px;
+  width: 167px;
+  :hover {
+    background: linear-gradient(
+      0deg,
+      rgba(42, 183, 248, 0.85) 27.3%,
+      rgba(127, 229, 255, 0) 173.47%
+    );
+    cursor: pointer;
+    transform: translate(0, 1px) scale(1.1, 1.1);
+  }
+`

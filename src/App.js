c7a348af30087a577c97ba4ca5b25919ae8c6427
Joe Previte <jjprevite@gmail.com>
2018-08-05 15:04:19 -0700

    update app styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
+import { css } from 'emotion'
 import Button from './components/Button'
 import Input from './components/Input'
 import './App.css'
 
+const appStyles = css`
+  width: 350px;
+  min-height: 350px;
+  background-color: #fff;
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 0px 24px 24px;
+`
 class App extends Component {
   state = { toggle: true }
   toggleToggle = () => this.setState({ toggle: !this.state.toggle })
@@ -10,8 +18,8 @@ class App extends Component {
     const { toggle } = this.state
     const currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className={appStyles}>
+        <header>
           <p>Your current timezone is</p>
           <h2>{currentTimezone}</h2>
         </header>

23537d3f2e8465ec82208760e2036a309474f4b3
Joe Previte <jjprevite@gmail.com>
2018-08-05 15:41:46 -0700

    write css for app/container and text in the header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react'
-import { css } from 'emotion'
 import Button from './components/Button'
 import Input from './components/Input'
 import './App.css'
+import { appStyles, headerStyles } from './App.styles'
 
-const appStyles = css`
-  width: 350px;
-  min-height: 350px;
-  background-color: #fff;
-  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
-  border-radius: 0px 0px 24px 24px;
-`
 class App extends Component {
   state = { toggle: true }
   toggleToggle = () => this.setState({ toggle: !this.state.toggle })
@@ -19,7 +12,7 @@ class App extends Component {
     const currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
     return (
       <div className={appStyles}>
-        <header>
+        <header className={headerStyles}>
           <p>Your current timezone is</p>
           <h2>{currentTimezone}</h2>
         </header>
---
+++ src/App.styles.js
@@ -0,0 +1,22 @@
+import { css } from 'emotion'
+
+export const appStyles = css`
+  width: 350px;
+  min-height: 350px;
+  background-color: #fff;
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 0px 24px 24px;
+  text-align: center;
+`
+export const headerStyles = css`
+    p {
+        margin-top: 37px;
+        margin-bottom: 0;
+    }
+
+    h2 {
+        font-size: 32px;
+        font-weight: 700;
+        margin-top: 6px;
+    }
+`

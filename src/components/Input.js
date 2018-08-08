df180c3c2a4b8a53070571327bb5c136a97b8f8b
Joe Previte <jjprevite@gmail.com>
2018-08-07 20:31:51 -0700

    add div around input and also add label

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
-
+import { inputBoxStyles } from './Input.styles'
 class Input extends Component {
-  render () {
-    return <input />
+  render() {
+    return (
+      <div className={inputBoxStyles}>
+        <input type={this.props.type} />
+        <label>{this.props.label}</label>
+      </div>
+    )
   }
 }
 

f59acc79b90bd5d2a94ed474d13500095261e665
Joe Previte <jjprevite@gmail.com>
2018-08-23 18:51:36 -0700

    remove rounded corners

--- src/App.styles.js
+++ src/App.styles.js
@@ -5,7 +5,6 @@ export const appStyles = css`
   min-height: 350px;
   background-color: #fff;
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
-  border-radius: 0px 0px 24px 24px;
   text-align: center;
 `
 export const headerStyles = css`

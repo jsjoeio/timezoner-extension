2c25342125f6ea11e9e45bef650a326f78870e8c
Joe Previte <jjprevite@gmail.com>
2018-08-25 14:00:34 -0700

    remove min-height and box shadow

--- src/App.styles.js
+++ src/App.styles.js
@@ -2,9 +2,7 @@ import { css } from 'emotion'
 
 export const appStyles = css`
   width: 350px;
-  min-height: 350px;
   background-color: #fff;
-  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
   text-align: center;
 `
 export const headerStyles = css`

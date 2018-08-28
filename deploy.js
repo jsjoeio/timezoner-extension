1b5049f00f321846558baaa626ce5f8e0ea1b1ac
Joe Previte <jjprevite@gmail.com>
2018-08-27 19:30:34 -0700

    repaste code

--- deploy.js
+++ deploy.js
@@ -8,59 +8,58 @@ const exec      = require('child_process').exec
 const folder = 'dist'
 const zipName = 'extension.zip'
 
-// credentials and IDs from travis.ci environment
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-const EXTENSION_ID = process.env.EXTENSION_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const CLIENT_ID = process.env.CLIENT_ID;
+// credentials and IDs from gitlab-ci.yml file
+let REFRESH_TOKEN = process.env.REFRESH_TOKEN
+let EXTENSION_ID = process.env.EXTENSION_ID
+let CLIENT_SECRET = process.env.CLIENT_SECRET
+let CLIENT_ID = process.env.CLIENT_ID
 
 // to fetch it from node_modules
-let webstoreLocation = './node_modules/.bin/webstore';
+let webstoreLocation = './node_modules/.bin/webstore'
 
-
-zipFolder(folder, zipName, function(err) {
+zipFolder(folder, zipName, function (err) {
   if (err) {
-    console.log('oh no!', err);
-    process.exit(1);
+    console.log('oh no!', err)
+    process.exit(1)
   } else {
-    console.log(`Successfully Zipped ${folder} and saved as ${zipName}`);
-    uploadZip(); // on successful zipping, call upload
+    console.log(`Successfully Zipped ${folder} and saved as ${zipName}`)
+    uploadZip() // on successful zipping, call upload
   }
 });
 
 function uploadZip() {
-  let cmd = getUploadCommand();
+  let cmd = getUploadCommand()
   exec(cmd, (error, stdout, stderr) => {
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
     if (error !== null) {
-      console.log(`exec error: ${error}`);
+      console.log(`exec error: ${error}`)
       process.exit(1);
     } else {
-      console.log('Successfully Uploaded the zip to chrome web store');
-      publishExtension(); // on successful upload, call publish
+      console.log('Successfully Uploaded the zip to chrome web store')
+      publishExtension() // on successful upload, call publish
     }
   });
 }
 
 function publishExtension() {
-  let cmd = getPublishCommand();
+  let cmd = getPublishCommand()
   exec(cmd, (error, stdout, stderr) => {
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
     if (error !== null) {
-      console.log(`exec error: ${error}`);
+      console.log(`exec error: ${error}`)
       process.exit(1);
     } else {
-      console.log('Successfully published the newer version');
+      console.log('Successfully published the newer version')
     }
   });
 }
 
 function getUploadCommand() {
-  return `${webstoreLocation} upload --source ${zipName} --extension-id ${EXTENSION_ID} --client-id ${CLIENT_ID} --client-secret ${CLIENT_SECRET} --refresh-token ${REFRESH_TOKEN}`;
+  return `${webstoreLocation} upload --source ${zipName} --extension-id ${EXTENSION_ID} --client-id ${CLIENT_ID} --client-secret ${CLIENT_SECRET} --refresh-token ${REFRESH_TOKEN}`
 }
 
 function getPublishCommand() {
-  return `${webstoreLocation} publish --extension-id ${EXTENSION_ID} --client-id ${CLIENT_ID} --client-secret ${CLIENT_SECRET} --refresh-token ${REFRESH_TOKEN}`;
+  return `${webstoreLocation} publish --extension-id ${EXTENSION_ID} --client-id ${CLIENT_ID} --client-secret ${CLIENT_SECRET} --refresh-token ${REFRESH_TOKEN}`
 }
